3be6492526e3a49fb106c44ca2151c5ae42480f0
sevaboh <61782735+sevaboh@users.noreply.github.com>
2026-05-14 12:02:21 +0300

    Update run_pt_tests.sh

--- run_pt_tests.sh
+++ run_pt_tests.sh
@@ -21,8 +21,8 @@ mv out_Vz.txt out_Vzpt.txt
 mv out_Z.txt out_Zpt.txt
 mv out_n.txt out_npt.txt
 
-g++ -O3 fr_soil_strain_stress.cpp -o fr_sss
-g++ -O3 -fopenmp fr_soil_strain_stress.cpp -o fr_sss_omp
+g++ -O3 fr_soil_strain_stress_v2.cpp -o fr_sss
+g++ -O3 -fopenmp fr_soil_strain_stress_v2.cpp -o fr_sss_omp
 time ./fr_sss_omp testing_mode 0 NB 150 debug_level 0 is_irr 1 b2 $biot2 biot $biot beta $order alpha $order Zoutstep 1 Tm 30000000 Om 3600 LB 1 pick_move 0.5 pick_eps 1e-10 ls_eps 1e-12 ls_e2 1e-14 fr_eps 1e-20 ls_max_iter 4000 vgm vgm2.txt et_file ET2.txt prec_file precipitation2.txt gw_file gw_depth3.txt max_tau 3600 st_tau 0.01 ls_min_tau 0.00000001 porosity 0.44 rho_s 1287 > out_omp.txt 
 mv out_H.txt out_Homp.txt
 mv out_V.txt out_Vomp.txt
